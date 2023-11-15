21dd406f675f92444c36ded9128d073c4c074a29
jt-lowe <92237655+jt-lowe@users.noreply.github.com>
2023-11-15 11:39:27 +1100

    Update streamlit_app.py
    adding error logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,15 +28,17 @@ streamlit.dataframe(fruits_to_show)
 
 #Python Packages
 streamlit.header('Fruityvice Fruit Advice')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice
+    streamlit.error("Please select a fruit to get information")
+  else
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# write your own comment -what does the next line do?  - analyses the json data that is pulled in by fruityvice
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do? - displays the fruityvice info on the app
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  strealit.error()
 
 streamlit.stop()
 
